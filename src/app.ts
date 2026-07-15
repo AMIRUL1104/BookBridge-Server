aad837e0c354849e9e9b5c329a288c6c8bfb3ddf
Amirul <amirulislam9.e@gmail.com>
2026-07-15 23:12:37 +0600

    update cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import routes from "./routes/index.js";
-
+import "dotenv/config";
 import express from "express";
 import type { Application, Request, Response } from "express";
 import cors from "cors";
@@ -7,7 +7,12 @@ import cors from "cors";
 const app: Application = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", process.env.CLIENT_URL!],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
